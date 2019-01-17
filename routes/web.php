8a0cf6a02f1461e8e60d88c43b941a8985b747f6
Dino16m <anselem16m@gmail.com>
2019-01-17 23:56:06 +0000

    3

--- routes/web.php
+++ routes/web.php
@@ -22,24 +22,10 @@ use Illuminate\Support\Facades\Hash;
 use App\User;
 
 Route::get('/me', function() {
-    //echo $y/ou.' are '. $me;
-    //User::create(['name'=>'dino','password'=>Hash::make('dino'), 'email'=>'anselem16m@gmail.com']);
+    
+    $ret = false;
+    return $ret;
 
-   //return strtolower( substr($RawVideoName, 3, (strpos($RawVideoName, '.')-3) ));
-    /**$val = pa////thinfo(parse_url($url, PHP_URL_PATH), PATHINFO_EXTENSION);
-   **/
-return mkdir(base_path('storage/tmp/e'));
-// $stream = file_get_contents(' http://waptrick.com/load-file/FF/55233/film/full3gphigh/Peshawar_Zalmi_Fours_Peshawar_Zalmi_Vs_lahore_Qalandars_Match_29_16_March_HBL_PSL_2018.3gp');
-/*$url ='http://d9.o2tvseries.com/Sex%20Education/Season%2001/Sex%20Education%20-%20S01E01%20(TvShows4Mobile.Com).3gp';
- $curl = curl_init(); 
-curl_setopt($curl, CURLOPT_URL, $url);
- curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
- curl_setopt($curl, CURLOPT_HEADER, false); 
-$stream = curl_exec($curl); curl_close($curl);  
-  $filepath = base_path('public/images/do.3gp');
-    $file = file_put_contents($filepath, $stream);
-    $File = new File($filepath);
-   return Storage::putFileAs('/public/', $File, 'fila.3gp' );
 /**$string = 'wwww.me.com/ypu/public/videos/love.mp4?';
 //$bool = preg_match('~/public/videos/~', $string);
 $match = [];
@@ -68,7 +54,7 @@ Route::get('recents/seemore', 'categoryController@getMoreRecents');
 //e.g udaratv.com/tags/comedy
 Route::get('tags/{Tag}', 'categoryController@getVideosOfTag');
 Route::get('genre/{Tag}', 'categoryController@getVideosOfTag');
-Route::get('tag/{Tag}', 'categoryController@getVideosOfTag'/);
+Route::get('tag/{Tag}', 'categoryController@getVideosOfTag');
 //e.g udaratv.com/types/naija
 Route::get('types/{Type}', 'categoryController@getVideosOfType');
 Route::get('type/{Type}', 'categoryController@getVideosOfType');
