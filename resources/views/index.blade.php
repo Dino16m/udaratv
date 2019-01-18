430467e2874fec6d1eebd0703d1ac9b936cc8b8b
Dino16m <anselem16m@gmail.com>
2019-01-18 00:13:32 +0000

    narrow escape

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -47,7 +47,7 @@ $lacarousel=json_encode($carousel);
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="google-site-verification" content="Th8HNdr7cKYDKfi21HsdFYYUa5mryCK_ahacx-i6BxE">
-    <meta name="description" content="Hollywood movies download, Tv series download, Nollywood movies download, UdaraTv.com, {{$carousel[0]['name']}}, {{$carousel[1]['name']}}, {{$carousel[2]['name']}}, {{$carousel[3]['name']}}">
+    <meta name="description" content="Hollywood movies download, Tv series download, Nollywood movies download, UdaraTv.com, {{$carousel[0]['name']}}, {{$carousel[1]['name']}}">
     <meta name="author" content="">
     <meta name="csrf-token" content="{{csrf_token()}}" >
     <link rel="icon" href="<?php echo url('public/images/udaralogo.png')?>">
