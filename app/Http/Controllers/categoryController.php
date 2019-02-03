f4b642068cf6aedd7501d3c12f11edaec5177645
Dino16m <anselem16m@gmail.com>
2019-02-03 12:08:08 +0000

    finally fixed UCweb download bug

--- app/Http/Controllers/categoryController.php
+++ app/Http/Controllers/categoryController.php
@@ -86,8 +86,9 @@ class categoryController extends Controller
         }
         $name = Validator::sanitize($name);
         $queryVids = $this::getSearch($name);
-        if(!$queryVids){ return response()->json(['error'=>$name.'not found']);}
+        if(!$queryVids){ return response()->json(['error'=>$name.' not found']);}
         $returnable = [];
+        $count =0;
         foreach ($queryVids as $queryVid) {
             # code...
             $thisname = $queryVid->name;
@@ -98,11 +99,12 @@ class categoryController extends Controller
             elseif (Constants::inMovie($type)) {
                 $link = url('movies/'.$type.'/'.$thisname);
             }
-            else{ return response()->json(['error'=>$name.'not found']);}
-            $returnable[$thisname]=[
+            else{ return response()->json(['error'=>$name.' not found']);}
+            $returnable[$count]=[
                 'name'=>$thisname,
                 'link'=>$link
             ];
+            $count++;
             
         }
         return response()->json($returnable);
@@ -199,10 +201,17 @@ class categoryController extends Controller
         $mime = $mime_type === false ? 'video/mp4' : $mime_type;
         $file_size = file_exists($base_path) ? filesize($base_path) : 0; 
         ob_end_clean();
-        $headers = array('Content-Type:'.$mime,
-                           'Content-Length:'.$file_size);
+        $headers = array('Content-Type'=>$mime,
+                           'Content-Length'=>$file_size);
         event( new downloadEvent(['type'=>$type, 'id'=>$id]));
+        $UA = strtolower($request->header('User-Agent'));
+        if(strpos($UA, 'ucbrowser') !==false && strpos($UA, 'mobile')!==false){
+            $to = url('storage/app'.$path);
+            return redirect()->away($to);
+        }
         return ($name && Storage::exists($path)) ? Storage::download($path, $name, $headers) : back();
+        //$to = url('storage/app'.$path);
+        //return redirect()->away($to);
     }
     public function getMovieIndex($Type, $Name)
     {
