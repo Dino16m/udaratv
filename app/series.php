8835c5b24f6601043e38ffe9d1902fe61c97f503
Dino16m <anselem16m@gmail.com>
2019-02-11 00:08:49 +0000

    correcting series model makeNew method

--- app/series.php
+++ app/series.php
@@ -17,7 +17,7 @@ class series extends Model
                             'episode_name'=>$details['episodeNumber'],'season_id'=>$season->id]);
      $seriesquality = $episode->quality()->create(['series_id'=>$this->id,'episodes_id'=>$episode->id,'quality'=>$details['quality'],'file_path'=>$details['file_path'],'season_number'=>$details['number_of_seasons'],'season_id'=>$season->id, 'number_downloaded'=> 0 ]);
      if( ($this!=null && $season!=null) && ($episode != null && $seriesquality != null)){
-         return ['series_id'=>$this->id, 'episode_id'=>$episode->id, $quality_id=>$seriesquality->id];
+         return ['series_id'=>$this->id, 'episode_id'=>$episode->id, 'quality_id'=>$seriesquality->id];
      }
      return false;
     }
